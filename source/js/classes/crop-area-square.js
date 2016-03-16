f374d84da0f079291a36339b231c6ebdd284e2fc
jose-granja <jose.granja.martinez@gmail.com>
2016-03-16 15:19:56 +0100

    fixing bug on coordinates

--- source/js/classes/crop-area-square.js
+++ source/js/classes/crop-area-square.js
@@ -75,7 +75,7 @@ crop.factory('cropAreaSquare', ['cropArea', function(CropArea) {
         for (var i = 0, len = resizeIconsCenterCoords.length; i < len; i++) {
             var resizeIconCenterCoords = resizeIconsCenterCoords[i];
             if (coord[0] > (resizeIconCenterCoords[0] - this._resizeCtrlHoverRadius - 15) && coord[0] < (resizeIconCenterCoords[0] + this._resizeCtrlHoverRadius + 15) &&
-                coord[1] > (resizeIconCenterCoords[1] - this._resizeCtrlHoverRadius - 15) && coord[1] < (resizeIconCenterCoords[1] + this._resizeCtrlHoverRadius - 15)) {
+                coord[1] > (resizeIconCenterCoords[1] - this._resizeCtrlHoverRadius - 15) && coord[1] < (resizeIconCenterCoords[1] + this._resizeCtrlHoverRadius + 15)) {
                 res = i;
                 break;
             }
